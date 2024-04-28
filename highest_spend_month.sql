c90a9642ab5c6c6a19fd6202975029f03207cb80
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2024-04-28 16:24:09 +0100

    feat: completed number 2

---
+++ highest_spend_month.sql
@@ -0,0 +1,21 @@
+USE sakila;
+
+CREATE OR REPLACE VIEW summed_up_transactions AS 
+SELECT 
+		MONTHNAME(STR_TO_DATE(transaction_date, '%d-%b-%y')) AS month,
+		card_type,
+		SUM(amount) as total_spendings
+FROM credit_card_transactions
+GROUP BY month, card_type;
+
+SELECT *
+FROM summed_up_transactions o
+WHERE total_spendings = (
+	SELECT 
+		MAX(total_spendings) AS max_spent
+    FROM summed_up_transactions i
+	GROUP BY card_type
+	HAVING o.total_spendings = max_spent AND o.card_type = i.card_type
+);
+
+DROP VIEW summed_up_transactions
